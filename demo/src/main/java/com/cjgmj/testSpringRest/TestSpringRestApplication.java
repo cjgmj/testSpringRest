b1048fe9dd97434895a757a2e88c3f89ab66847f
cjgmj <cjgmj@hotmail.com>
2018-10-12 14:30:21 +0200

    Proyecto base

---
+++ demo/src/main/java/com/cjgmj/testSpringRest/TestSpringRestApplication.java
@@ -0,0 +1,12 @@
+package com.cjgmj.testSpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringRestApplication.class, args);
+	}
+}
---
+++ demo/src/test/java/com/cjgmj/testSpringRest/TestSpringRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cjgmj.testSpringRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSpringRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
