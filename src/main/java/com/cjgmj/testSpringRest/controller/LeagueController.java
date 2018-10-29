b9ca4ccddcf92a9bb59dae3e998bf2b1d0bc36fb
cjgmj <cjgmj@hotmail.com>
2018-10-29 16:59:16 +0100

    Cambios controladores

--- src/main/java/com/cjgmj/testSpringRest/controller/LeagueController.java
+++ src/main/java/com/cjgmj/testSpringRest/controller/LeagueController.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cjgmj.testSpringRest.entity.League;
 import com.cjgmj.testSpringRest.service.LeagueService;
 
-@Controller
+@RestController
 @RequestMapping("/leagues")
 public class LeagueController {
 	
